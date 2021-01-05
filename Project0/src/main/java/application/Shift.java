48eca669523611bc765def5ba40cc817e22305be
apst2020-01 <76921334+apst2020-01@users.noreply.github.com>
2021-01-05 17:16:45 +0200

    Add files via upload

---
+++ Project0/src/main/java/application/Shift.java
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Faculty Training Institute (FTI) 
+ *  Project 0:  Hello World Application.
+ *--------------------------------------------------------------------------------------------*/
+
+package application;
+public class Shift {
+
+	public static void main(String[] args) {
+		 System.out.println( "Hello World!" );
+	}
+}
